0a32981e29e80a66f30a194f087049785f942c05
Schwob42 <tim-fabian@gmx.net>
2024-02-02 10:42:31 +0100

    Update Maze

--- VR FireFighters/Assets/MazeGenerator/Scripts/Game.cs
+++ VR FireFighters/Assets/MazeGenerator/Scripts/Game.cs
@@ -4,11 +4,15 @@ using Unity.Mathematics;
 using UnityEngine;
 
 using static Unity.Mathematics.math;
+using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
 	[SerializeField]
 	MazeVisualization visualization;
+
+	[SerializeField, Tooltip("Use zero for random seed. Tutorial will use 123")]
+	int seed;
 
 	[SerializeField]
 	int2 mazeSize = int2(20, 20);
@@ -18,6 +22,13 @@ public class Game : MonoBehaviour
 	void Awake()
 	{
 		maze = new Maze(mazeSize);
+
+		new GenerateMazeJob
+		{
+			maze = maze,
+			seed = seed != 0 ? seed : Random.Range(1, int.MaxValue)
+		}.Schedule().Complete();
+
 		visualization.Visualize(maze);
 	}
 
